b2433f2b3b8370dbfd165d99aed95fe121970b19
Tristan Emmerson <tristan@stickerslug.com>
2020-07-20 14:52:17 -0700

    complete second madlib template and implement all necessary functionality unstyled. Co-authored-by: Sara Kane <kane.sarak@gmail.com> Co-authored-by: Tristan Emmerson <tristan@stickerslug.com> Co-authored-by: Christine Augustine <blacchoy@gmail.com>

--- MadLib/Controllers/HomeController.cs
+++ MadLib/Controllers/HomeController.cs
@@ -8,6 +8,9 @@ namespace MadLib.Controllers
     [Route("/")]
     public ActionResult Form() { return View(); }
 
+    [Route("/1")]
+    public ActionResult NewForm() { return View(); }
+
     [Route("/story")]
     public ActionResult Story(string person, string adjective1, string adjective2, string adjective3, string noun1, string noun2, string color, string verb1, string verb2, string verb3, string verb4) 
     { 
@@ -24,6 +27,27 @@ namespace MadLib.Controllers
       userWordVariable.Verb3 = verb3;
       userWordVariable.Verb4 = verb4; 
       return View(userWordVariable); 
+    }
+
+    [Route("/newstory")]
+    public ActionResult NewStory(string name, string spaceplace1, string number, string emotion1, string emotion2, string miss1, string miss2, string spaceattraction, string color, string spaceplace2, string bignumber, string animalplural, string thing, string homeplanet) 
+    { 
+      NewWordVariables userNewWordVariables = new NewWordVariables();
+      userNewWordVariables.Name = name;
+      userNewWordVariables.SpacePlace1 = spaceplace1;
+      userNewWordVariables.Number = number;
+      userNewWordVariables.Emotion1 = emotion1;
+      userNewWordVariables.Emotion2 = emotion2;
+      userNewWordVariables.Miss1 = miss1;
+      userNewWordVariables.Miss2 = miss2;
+      userNewWordVariables.SpaceAttraction = spaceattraction;
+      userNewWordVariables.Color = color;
+      userNewWordVariables.SpacePlace2 = spaceplace2;
+      userNewWordVariables.BigNumber = bignumber;
+      userNewWordVariables.AnimalPlural = animalplural;
+      userNewWordVariables.Thing = thing;
+      userNewWordVariables.HomePlanet = homeplanet;       
+      return View(userNewWordVariables); 
     }
   }
   
