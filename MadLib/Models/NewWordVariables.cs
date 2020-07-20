a0e4857269e99be77718bef387c3e319a1d087c2
Tristan Emmerson <tristan@stickerslug.com>
2020-07-20 14:14:41 -0700

    add additional files for second space-themed madlib template.. Co-authored-by: Sara Kane <kane.sarak@gmail.com> Co-authored-by: Tristan Emmerson <tristan@stickerslug.com> Co-authored-by: Christine Augustine <blacchoy@gmail.com>

---
+++ MadLib/Models/NewWordVariables.cs
@@ -0,0 +1,20 @@
+namespace MadLib.Models
+{
+  public class NewWordVariables
+  {
+    public string Name { get; set;}
+    public string SpacePlace1 { get; set; }
+    public string Number { get; set; }
+    public string Emotion1 { get; set; }
+    public string Emotion2 { get; set; }
+    public string Miss1 { get; set; }
+    public string Miss2 { get; set; }
+    public string SpaceAttraction { get; set; }
+    public string Color { get; set; }
+    public string SpacePlace2 { get; set; }
+    public string BigNumber { get; set; }
+    public string AnimalPlural { get; set; }
+    public string Thing { get; set; }
+    public string HomePlanet { get; set; }
+  }
+}
