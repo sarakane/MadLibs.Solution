772004401b7c3b1e87b8aa3e5f1afa1db860e661
Tristan Emmerson <tristan@stickerslug.com>
2020-07-20 11:56:45 -0700

    add startup and program cs files and updated corresponding namespace references to project name. Co-authored-by: Sara Kane <kane.sarak@gmail.com> Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>

--- MadLib/Controllers/HomeController.cs
+++ MadLib/Controllers/HomeController.cs
@@ -3,16 +3,26 @@ using MadLib.Models;
 
 namespace MadLib.Controllers
 {
-  public class HomeController : Controllers
+  public class HomeController : Controller
   {
-    [Route("/form")]
+    [Route("/")]
     public ActionResult Form() { return View(); }
 
     [Route("/story")]
-    public ActionResult Story() 
+    public ActionResult Story(string person, string adjective1, string adjective2, string adjective3, string noun1, string noun2, string color, string verb1, string verb2, string verb3, string verb4) 
     { 
-      WordVariable userWordVariable = new WordVariable();
-      userWordVariable.
+      WordVariables userWordVariable = new WordVariables();
+      userWordVariable.Person = person;
+      userWordVariable.Adjective1 = adjective1;
+      userWordVariable.Adjective2 = adjective2;
+      userWordVariable.Adjective3 = adjective3;
+      userWordVariable.Noun1 = noun1;
+      userWordVariable.Noun2 = noun2;
+      userWordVariable.Color = color;
+      userWordVariable.Verb1 = verb1;
+      userWordVariable.Verb2 = verb2;
+      userWordVariable.Verb3 = verb3;
+      userWordVariable.Verb4 = verb4;
       return View(); 
       }
   }
